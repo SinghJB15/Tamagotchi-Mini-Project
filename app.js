eea694b3d88155ddfcc05edeed53abc1221ff754
bhogalj15 <jazz3806@gmail.com>
2023-10-05 01:44:24 -0400

    added methods to increase metric values

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ class Tamagotchi {
         this.name = name;
         this.hunger = 0;
         this.sleepiness = 0;
-        this.boredom = 0
+        this.boredom = 0;
+        this.age = 0;
+    }
+
+    increaseAge() {
+        this.age++;
+    }
+
+    increaseHunger() {
+        if(this.hunger < 10) {
+            this.hunger++;
+        }
+    }
+
+    increaseSleepiness() {
+        if(this.sleepiness < 10) {
+            this.sleepiness++;
+        }
+    }
+
+    increaseBoredom() {
+        if(this.boredom < 10) {
+            this.boredom++
+        }
     }
 }
